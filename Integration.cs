291ad890bcc3c9869fdc30c340e12e48d9a75afa
Callombus <pb_reg@mail.ru>
2017-10-20 16:36:51 +0300

    Add files via upload

---
+++ Integration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    public class Integration
+    {
+ 
+     public double function(double x, int k)
+        {
+             switch (k)
+            {
+             case 1: return Math.Sin(x);
+             case 2: return Math.Cos(x);
+             case 3: return Math.Pow(x, 3) + 1;
+             case 4: return Math.Pow(x, 2) - Math.Pow(x, 1);
+             case 5: return Math.Pow(x, 3) + Math.Pow(x, 2);
+             
+
+             default:
+                 return x;
+                    }
+ 
+ 
+     }
+ 
+     public double Rectangle(int k, double a, double b, int n)
+    {
+
+            double h = (b - a) / n;
+            double sum = 0;
+            int i;
+            for (i = 0; i <= n; i++)
+            {
+             sum += function(a + h * (i + 0.5), k);
+             }
+
+        
+                return sum * h;
+            }
+ 
+     public double Trapezia(int k, double a, double b, int n)
+    {
+            double h = (b - a) / n;
+            double sum = 0;
+            double x;
+            int i;
+            for (i = 1; i <= n; i++)
+                        {
+                x = a + i * h;
+                sum += function(x, k);
+                        }
+            return h * (sum + (function(a, k) + function(b, k))/2);
+            }
+ 
+     public double Simpson(int k, double a, double b, int n)
+    {
+            double h = (b - a) / n;
+            double sum = 0;
+            double x;
+                for (x = a; x <= b - 2 * h; x += 2 * h)
+        {
+            sum += 2 * function(x + 2 * h, k) +
+             +4 * function(x + h, k);
+                    }
+                return h / 3 * (sum + function(a, k) - function(b, k));
+            }
+ }
+}
---
+++ Tests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+            Integration intgr = new Integration();
+            double e = 0.01;
+
+
+            if (Math.Abs(intgr.Rectangle(1, 1, 5, 1000)) - 0.2566 > e)
+                Console.WriteLine("Неверное значение для функции 1");
+            else Console.WriteLine("Верное значение для функции 1");
+            if (Math.Abs(intgr.Trapezia(1, 1, 5, 1000)) - 0.2566 > e)
+                Console.WriteLine("Неверное значение для функции 1");
+            else Console.WriteLine("Верное значение для функции 1");
+            if (Math.Abs(intgr.Simpson(1, 1, 5, 1000)) - 0.2566 > e)
+                Console.WriteLine("Неверное значение для функции 1");
+            else Console.WriteLine("Верное значение для функции 1");
+
+
+            if (Math.Abs(intgr.Rectangle(2, 1, 5, 1000)) - 1.8004 > e)
+                Console.WriteLine("Неверное значение для функции 2");
+            else Console.WriteLine("Верное значение для функции 2");
+            if (Math.Abs(intgr.Trapezia(2, 1, 5, 1000)) - 1.8004 > e)
+                Console.WriteLine("Неверное значение для функции 2");
+            else Console.WriteLine("Верное значение для функции 2");
+            if (Math.Abs(intgr.Simpson(2, 1, 5, 1000)) - 1.8004 > e)
+                Console.WriteLine("Неверное значение для функции 2");
+            else Console.WriteLine("Верное значение для функции 2");
+
+
+            if (Math.Abs(intgr.Rectangle(3, 1, 5, 1000)) - 160 > e)
+            Console.WriteLine("Неверное значение для функции 3");
+            else Console.WriteLine("Верное значение для функции 3");
+            if (Math.Abs(intgr.Trapezia(3, 1, 5, 1000)) - 160 > e)
+                Console.WriteLine("Неверное значение для функции 3");
+            else Console.WriteLine("Верное значение для функции 3");
+            if (Math.Abs(intgr.Trapezia(3, 1, 5, 1000)) - 160 > e)
+                Console.WriteLine("Неверное значение для функции 3");
+            else Console.WriteLine("Верное значение для функции 3");
+
+
+            if (Math.Abs(intgr.Rectangle(4, 1, 5, 1000)) - 29.33 > e)
+                Console.WriteLine("Неверное значение для функции 4");
+            else Console.WriteLine("Верное значение для функции 4");
+            if (Math.Abs(intgr.Trapezia(4, 1, 5, 1000)) - 29.33 > e)
+                Console.WriteLine("Неверное значение для функции 4");
+            else Console.WriteLine("Верное значение для функции 4");
+            if (Math.Abs(intgr.Simpson(4, 1, 5, 1000)) - 29.33 > e)
+                Console.WriteLine("Неверное значение для функции 4");
+            else Console.WriteLine("Верное значение для функции 4");
+
+
+            if (Math.Abs(intgr.Rectangle(5, 1, 5, 1000)) - 197.33 > e)
+                Console.WriteLine("Неверное значение для функции 5");
+            else Console.WriteLine("Верное значение для функции 5");
+            if (Math.Abs(intgr.Trapezia(5, 1, 5, 1000)) - 197.33 > e)
+                Console.WriteLine("Неверное значение для функции 5");
+            else Console.WriteLine("Верное значение для функции 5");
+            if (Math.Abs(intgr.Trapezia(5, 1, 5, 1000)) - 197.33 > e)
+                Console.WriteLine("Неверное значение для функции 5");
+            else Console.WriteLine("Верное значение для функции 5");
+
+            Console.ReadLine();
+
+        }
+    }
+}
